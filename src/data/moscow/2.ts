10085f865fd75cc77085c2c02ad4df6b469d74ca
mislaurov said <saidmislaur@gmail.com>
2024-01-29 10:01:35 +0300

    Create 2.ts

---
+++ src/data/moscow/2.ts
@@ -0,0 +1,256 @@
+import { DailySchedule } from '../../types'
+
+export const schedule: DailySchedule[] = [
+    {
+        day: 1,
+        fajr: "06:15",
+        sunrise: "08:18",
+        zuhr: "12:49",
+        asr: "14:42",
+        maghrib: "17:09",
+        isha: "18:51"
+    },
+    {
+        day: 2,
+        fajr: "06:13",
+        sunrise: "08:16",
+        zuhr: "12:49",
+        asr: "14:44",
+        maghrib: "17:11",
+        isha: "18:52"
+    },
+    {
+        day: 3,
+        fajr:"06:12",
+        sunrise:"08:14",
+        zuhr:"12:49",
+        asr:"14:45",
+        maghrib:"17:13",
+        isha:"18:54"
+    },
+    {
+        day: 4,
+        fajr: "06:10",
+        sunrise: "08:13",
+        zuhr: "12:49",
+        asr: "14:47",
+        maghrib: "17:15",
+        isha: "18:56"
+    },
+    {
+        day: 5,
+        fajr: "06:09",
+        sunrise:"08:11",
+        zuhr:"12:49",
+        asr:"14:49",
+        maghrib:"17:17",
+        isha:"18:58"
+    },
+    {
+        day: 6,
+        fajr:"06:07",
+        sunrise:"08:09",
+        zuhr:"12:49",
+        asr:"14:50",
+        maghrib:"17:19",
+        isha:"19:00"
+    },
+    {
+        day: 7,
+        fajr: "06:05",
+        sunrise: "08:07",
+        zuhr: "12:49",
+        asr: "14:52",
+        maghrib: "17:21",
+        isha: "19:02"
+    },
+    {
+        day: 8,
+        fajr: "06:03",
+        sunrise: "08:04",
+        zuhr: "12:49",
+        asr: "14:54",
+        maghrib: "17:24",
+        isha: "19:03"
+    },
+    {
+        day: 9,
+        fajr: "06:02",
+        sunrise: "08:02",
+        zuhr: "12:49",
+        asr: "14:55",
+        maghrib: "17:26",
+        isha: "19:05"
+    },
+    {
+        day: 10,       
+        fajr: "06:00",
+        sunrise: "08:00",
+        zuhr: "12:49",
+        asr: "14:57",
+        maghrib: "17:28",
+        isha: "19:07"
+    },
+    {
+        day: 11,       
+        fajr: "05:58",
+        sunrise: "07:58",
+        zuhr: "12:49",
+        asr: "14:59",
+        maghrib: "17:30",
+        isha: "19:09"
+    },
+    {
+        day: 12,       
+        fajr: "05:56",
+        sunrise: "07:56",
+        zuhr: "12:49",
+        asr: "15:00",
+        maghrib: "17:32",
+        isha: "19:11"
+    },
+    {
+        day: 13,       
+        fajr: "05:54",
+        sunrise: "07:54",
+        zuhr: "12:49",
+        asr: "15:02",
+        maghrib: "17:35",
+        isha: "19:13"
+    },
+    {
+        day: 14,       
+        fajr: "05:52",
+        sunrise: "07:51",
+        zuhr: "12:49",
+        asr: "15:04",
+        maghrib: "17:37",
+        isha: "19:15"
+    },
+    {
+        day: 15,       
+        fajr: "05:50",
+        sunrise: "07:49",
+        zuhr: "12:49",
+        asr: "15:05",
+        maghrib: "17:39",
+        isha: "19:17"
+    },
+    {
+        day: 16,       
+        fajr: "05:48",
+        sunrise: "07:47",
+        zuhr: "12:49",
+        asr: "15:07",
+        maghrib: "17:41",
+        isha: "19:19"
+    },
+    {
+        day: 17,       
+        fajr: "05:46",
+        sunrise: "07:45",
+        zuhr: "12:49",
+        asr: "15:09",
+        maghrib: "17:43",
+        isha: "19:21"
+    },
+    {
+        day: 18,       
+        fajr: "05:44",
+        sunrise: "07:42",
+        zuhr: "12:49",
+        asr: "15:10",
+        maghrib: "17:45",
+        isha: "19:23"
+    },
+    {
+        day: 19,       
+        fajr: "05:42",
+        sunrise: "07:40",
+        zuhr: "12:49",
+        asr: "15:12",
+        maghrib: "17:48",
+        isha: "19:25"
+    },
+    {
+        day: 20,       
+        fajr: "05:40",
+        sunrise: "07:38",
+        zuhr: "12:49",
+        asr: "15:14",
+        maghrib: "17:50",
+        isha: "19:27"
+    },
+    {
+        day: 21,       
+        fajr: "05:37",
+        sunrise: "07:35",
+        zuhr: "12:49",
+        asr: "15:15",
+        maghrib: "17:52",
+        isha: "19:29"
+    },
+    {
+        day: 22,       
+        fajr: "05:35",
+        sunrise: "07:33",
+        zuhr: "12:49",
+        asr: "15:17",
+        maghrib: "17:54",
+        isha: "19:31"
+    },
+    {
+        day: 23,       
+        fajr: "05:33",
+        sunrise: "07:31",
+        zuhr: "12:48",
+        asr: "15:18",
+        maghrib: "17:56",
+        isha: "19:33"
+    },
+    {
+        day: 24,       
+        fajr: "05:30",
+        sunrise: "07:28",
+        zuhr: "12:48",
+        asr: "15:20",
+        maghrib: "17:58",
+        isha: "19:35"
+    },
+    {
+        day: 25,       
+        fajr: "05:28",
+        sunrise: "07:26",
+        zuhr: "12:48",
+        asr: "15:21",
+        maghrib: "18:00",
+        isha: "19:37"
+    },
+    {
+        day: 26,       
+        fajr: "05:26",
+        sunrise: "07:23",
+        zuhr: "12:48",
+        asr: "15:23",
+        maghrib: "18:03",
+        isha: "19:39"
+    },
+    {
+        day: 27,       
+        fajr: "05:23",
+        sunrise: "07:21",
+        zuhr: "12:48",
+        asr: "15:25",
+        maghrib: "18:05",
+        isha: "19:41"
+    },
+    {
+        day: 28,
+        fajr: "05:21",
+        sunrise: "07:18",
+        zuhr: "12:48",
+        asr: "15:26",
+        maghrib: "18:07",
+        isha: "19:43"
+    },
+]
